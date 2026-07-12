855a13aad0b3ec4c40cd2bfea00d7a4036d205fc
kowaldiv <pochtaniki8@gmail.com>
2026-07-12 13:59:41 +0300

    небольшие проблемы с ui на главной странице при уменьшении экрана починил

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,5 +1,5 @@
 import { useNavBarStore } from "@/src/stores/nav-bar-store";
-import { Chat } from "./chat/Chat";
+import { Chat } from "./chat/Chatas";
 import { NavBar } from "./nav-bar/NavBar";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="max-w-dvw overflow-hidden">
       <div
-        className={`flex min-w-[200dvw] sm:min-w-dvh h-dvh transition-transform duration-300 sm:translate-x-0 ${
+        className={`flex min-w-[200dvw] sm:min-w-dvw h-dvh transition-transform duration-300 sm:translate-x-0 ${
           isNavBarHidden ? "-translate-x-[100dvw]" : "translate-x-0"
         }`}
       >
